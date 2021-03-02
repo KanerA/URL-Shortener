91cb0cb48abe23d51178f0de5160fc8b79dccbbe
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-02 23:15:33 +0200

    create DB class

---
+++ dataBase.js
@@ -0,0 +1,16 @@
+const fsPromises = require('fs').promises;
+const shortId = require('shortid');
+const dir = process.env.NODE_ENV === 'test' ? './test':'./data';
+
+class DB{
+    static urls = [];
+    
+    // add the url given to the database
+    
+    // redirect with the short url
+
+    // read all data and add new urls
+    
+}
+
+module.exports = DB;
