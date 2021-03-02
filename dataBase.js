c6aa34e2d93333f2138f46b21ec823a66333bae8
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-02 23:24:53 +0200

    add read data static method
    this method will read the data from the data file and will be used in the add url method.

--- dataBase.js
+++ dataBase.js
@@ -10,7 +10,16 @@ class DB{
     // redirect with the short url
 
     // read all data and add new urls
-    
+    static readData(){
+        try{
+            fsPromises.readFile(`${dir}/data.json`, 'utf-8')
+            .then((res) => {
+                this.urls = res;
+            })
+        } catch(e) {
+            console.log(e);
+        }
+    }
 }
 
 module.exports = DB;
