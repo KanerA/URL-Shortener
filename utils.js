ec98aa495a2d755c77338c827fc18d4cc34f6521
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-03 21:11:44 +0200

    add try/catch to utils functions

--- utils.js
+++ utils.js
@@ -3,36 +3,43 @@ const DB = require("./dataBase");
 
 const checkIdValid = (req, res, next) => {
     const { id } = req.params;
-    if(!shortid.isValid(id)) res.status(400).json({message: "Invalid Bin Id provided"})
+    if(!shortid.isValid(id)) return res.status(400).json({message: "Invalid Bin Id provided"})
     next();
 }
 
 const checkIdExist = async (req, res, next) => {
     const { id } = req.params;
-    await DB.readData();
-    let counter = 0;
-    for(url of DB.urls){
-        if(id !== url.shortUrlId){
-            counter++;
+    try{
+        await DB.readData();
+        let counter = 0;
+        for(url of DB.urls){
+            if(id !== url.shortUrlId){
+                counter++;
+            }
         }
+        if(counter === DB.urls.length) return res.status(404).json({message: "id not found"})
+        next();
+    } catch(e){
+        console.log(e);
     }
-    if(counter === DB.urls.length) res.status(404).json({message: "id not found"})
-    next();
 }
 
 const urlExist = async (req, res, next) => {
     const { url } = req.body;
-    console.log(url);
-    await DB.readData();
-    for(let savedUrl of DB.urls){
-        if(url === savedUrl.originalUrl){
-            return res.json({
-                message: "URL already shortened",
-                shortUrl: savedUrl.shortUrlId
-            })
+    try{
+        await DB.readData();
+        for(let savedUrl of DB.urls){
+            if(url === savedUrl.originalUrl){
+                return res.json({
+                    message: "URL already shortened",
+                    shortUrl: savedUrl.shortUrlId
+                })
+            }
         }
+        next();
+    } catch(e){
+        console.log(e);
     }
-    next();
 }
 
-module.exports = { checkIdExist, checkIdValid, urlExist };
+module.exports = { checkIdExist, checkIdValid, urlExist, checkIfBlank };
