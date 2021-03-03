21d0787931dbeca5b651de11272a5a61988d91a6
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-03 12:54:09 +0200

    import the DB class & urlencoded

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { urlencoded } = require('body-parser');
+const  DB  = require('./dataBase');
 
 app.use(cors());
-
 app.use("/public", express.static(`./public`));
+app.use(urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
