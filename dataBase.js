d07fd50531510800ee9940c8124e2b21dc223485
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-03 12:42:48 +0200

    create addURL static nethod
    this method adds the url entered, creation date, redirect count and the new url ,to the database

--- dataBase.js
+++ dataBase.js
@@ -6,15 +6,31 @@ class DB{
     static urls = [];
     
     // add the url given to the database
-    
+    static async addURL({ body }){
+        const data = {
+            creationDate: Date.now(),
+            redirectCount: 0,
+            originalUrl: body.url,
+            shortUrlId: shortId.generate()
+        }
+        try{
+            await DB.readData();
+            DB.urls.push(data);
+            await fsPromises.writeFile(`${dir}/data.json`, JSON.stringify(DB.urls, null, 4));
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
     // redirect with the short url
 
     // read all data and add new urls
-    static readData(){
+    static async readData(){
         try{
-            fsPromises.readFile(`${dir}/data.json`, 'utf-8')
+            await fsPromises.readFile(`${dir}/data.json`, 'utf-8')
             .then((res) => {
-                this.urls = res;
+                const parsed = JSON.parse(res);
+                this.urls = parsed;
             })
         } catch(e) {
             console.log(e);
