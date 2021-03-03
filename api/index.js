0b69649eda574a94da3247a54fc8f4311270d8fd
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-03 22:34:21 +0200

    change file name
    statistic --> statistics

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const shorturl = require("./shorturl");
-const statistic = require("./statistic");
+const statistics = require("./statistics");
 const DB = require('../dataBase');
 const { checkIdExist } = require('../utils');
 const { checkIdValid } = require('../utils');
@@ -8,7 +8,7 @@ const { checkIdValid } = require('../utils');
 const api = Router();
 
 api.use("/shorturl", shorturl);
-api.use("/statistic", statistic);
+api.use("/statistics", statistics);
 api.get('/:id', checkIdValid, checkIdExist, DB.redirectUrl);
 
 module.exports = api;
