c89011f8d20c7739092eeb9ed11561db154463af
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-04 09:04:13 +0200

    Update index.js/statistics

--- api/statistics/index.js
+++ api/statistics/index.js
@@ -1,7 +1,14 @@
 const { Router } = require("express");
+const DB = require('../../dataBase')
 
 const statistics = Router();
 
+
+statistics.get("/:id", DB.getIdStatistics);
+
+statistics.get("/", (req, res) => {
+  res.render('statistics');
+});
 
 statistics.use("*", (req, res) => {
   res.status(404).send(({"message": "Page Not Found"}));
