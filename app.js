867705769ac05e2e7a4b23b346abab585fb160ed
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-04 09:09:24 +0200

    add index.pug as homepage

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const api = require("./api");
 const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use("/api", api);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.render('index');
 });
+
+
 
 module.exports = app;
 
