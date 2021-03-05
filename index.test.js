a9a6f3f934b765f1743faf2fafc00170c7d5af47
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-05 22:30:44 +0200

    add expect to first test
    this will check how much objects are in the database file before and after the request.

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require("./app.js");
 const fs = require('fs');
 const dir = "./test";
+const DB = require('./dataBase');
 
 const URL = {
     url : "https://www.google.com"
@@ -19,9 +20,13 @@ beforeAll(() => {
 
 describe("test post", () => {
     it("should save the url in a database", async () => {
+        await DB.readData();
+        const expectedLength = DB.urls.length + 1;
         const res = await request(app).post('/api/shorturl').type('form').send(URL);
-        expect(res.status).toBe(200);
-        expect(res.body.originalUrl).toEqual(URL.url);
+        await DB.readData();
+        expect(DB.urls.length).toBe(expectedLength); // test in the data base
+        expect(res.status).toBe(200); // test the response status
+        expect(res.body.originalUrl).toEqual(URL.url); // test if the correct url was saved
     });
 
     test('If an error message returns when url already exists', async () => {
