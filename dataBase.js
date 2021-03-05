7b2391837a00eccf06dbf0d739b7e07094092063
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-05 17:26:27 +0200

    send render response for addUrl
    this will show an html page with the new created URL

--- dataBase.js
+++ dataBase.js
@@ -18,10 +18,8 @@ class DB {
             await DB.readData();
             DB.urls.push(data);
             await fsPromises.writeFile(`${dir}/data.json`, JSON.stringify(DB.urls, null, 4));
-            res.status(200).json({
-                message: "short url created",
-                shortUrl: `http://localhost:3000/api/${data.shortUrlId}`
-            })
+            data.shortUrl = `http://localhost:3000/api/${data.shortUrlId}`;
+            res.status(200).render('shortUrl', data)
         } catch(e) {
             console.log(e);
         }
